a801a5d4a0e395e00a4cd380f99c812322e04f35
chacha255 <salsabilasy@students.amikom.ac.id>
2022-03-17 15:44:56 +0700

    inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
